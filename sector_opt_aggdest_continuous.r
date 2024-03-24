f01e0daf3a8c8a1363ccadb0f070336f89a5253c
datadrivensupplychain <76882555+datadrivensupplychain@users.noreply.github.com>
2024-03-24 15:36:59 -0500

    Update sector_opt_aggdest_continuous.r
    including RSS as decision variable

--- sector_opt_aggdest_continuous.r
+++ sector_opt_aggdest_continuous.r
@@ -16,7 +16,7 @@ array_2d_multiplication_fcn <- function(static_array, row_variable, column_varia
   vapply(seq_along(static_array), function(k) static_array[
     row_variable[k], column_variable[k]], numeric(1L))  }
 
-highs_solver_parameters <- list(mip_rel_gap=0.1,log_to_console=TRUE)
+highs_solver_parameters <- list(mip_rel_gap=0.25,log_to_console=TRUE,time_limit = 180)
 avg_sector_metric_pct_tolerance <- 0.10  #set tighter than the actual tolerance. Need some wiggle room
 #for zips that are assigned to multiple sectors
 
@@ -28,7 +28,7 @@ sector_count <- 48 #48 prod recruiters + 12 SNCOICs in RS TWC
 all_zips <- readxl::read_excel("C:/Users/r_j_a/Documents/USMC/MCRC Structure/FY20 - FY35 MCRC Structure Data for Distribution (Feb 2024).xlsx", 
                                   sheet = "MCRC Zip DATA")
 
-sector_zips <- all_zips %>%
+rs_zips <- all_zips %>%
   janitor::clean_names() %>%
   dplyr::select(rs,rss,state,county,fips,zip,lat,long,metric=qep_2025) %>%
   dplyr::filter(rs == 'TWIN CITIES') %>%
@@ -39,35 +39,45 @@ sector_zips <- all_zips %>%
   dplyr::filter(metric>0) %>%
   dplyr::mutate(zip = as.character(zip))
 
+rss_df <- cbind.data.frame(
+  rss = c('BLOOMINGTON','BUFFALO','BURNSVILLE','COON RAPIDS','DULUTH','EAU CLAIRE','FARGO','MANKATO','ROSEVILLE','SIOUX CITY','SIOUX FALLS','ST CLOUD'),
+  rss_abb = c('BL','BF','BU','CO','DU','EC','FA','MK','RV','SY','SF','SC'),
+  lat = c(44.8114267,45.1782933,44.7733899,45.1740642,46.8049533,44.8071073,46.8640532,44.1728541,45.0060767,42.4503635,43.4994024,45.5537568),
+  lng = c(-93.3291527,-93.8667234,-93.2766312,-93.3444056,-92.166503,-91.468971,-96.8528707,-93.9537317,-93.1566107,-96.352813,-96.7728009,-94.1913827),
+  sectors = c(4,3,4,5,3,5,5,3,4,3,5,4),
+  sncoic = c(1,1,1,1,1,1,1,1,1,1,1,1),
+  rop = c(5,4,5,6,4,6,6,4,5,4,6,5)
+)
 
-
-#sector_zips <- zipcodeR::zip_code_db %>% dplyr::filter(state=='MN')%>%
-#  dplyr::select(zipcode,state,lat,lng,population) %>%
-#  dplyr::mutate(zip4=substr(zipcode,1,4),
-#                zip3=substr(zipcode,1,3)
- #               ) %>%
-#  dplyr::filter(!is.na(lat),!is.na(lng),!is.nan(lat),!is.nan(lng)) %>%
-#  dplyr::filter(population>0)
 
 #summarize destinations at zip4 level
-dest_summarized <- sector_zips %>% 
+dest_summarized <- rs_zips %>% 
   dplyr::group_by(zip4) %>%
   dplyr::summarise(lat=sum(lat*metric)/sum(metric),
                    long=sum(long*metric)/sum(metric)) %>%
   dplyr::rename(zip=zip4) 
   
-zipcount <- nrow(sector_zips)
+
+
+
+zipcount <- nrow(rs_zips)
 destcount <- nrow(dest_summarized)
+rsscount <- nrow(rss_df)
+
+bigM <- sum(rs_zips$metric) * 1.1
+avg_sector_metric <- sum(rs_zips$metric)/sector_count
 
 
-bigM <- sum(sector_zips$metric) * 1.1
-avg_sector_metric <- sum(sector_zips$metric)/sector_count
-
-
-#distance matrix, weighted by metric, for objective function
+#zip to sector dest distance matrix, multiplied by volume for objective function
 zip_dest_dist <- geosphere::distm(
-  x=cbind(sector_zips$long,sector_zips$lat),
+  x=cbind(rs_zips$long,rs_zips$lat),
   y=cbind(dest_summarized$long,dest_summarized$lat)) %>%
+  measurements::conv_unit('m','mi') * 0.0001 #scale down for numeric performance in solver
+
+#sector dest to rss distance matrix, multiplied by volume for objective function
+dest_rss_dist <- geosphere::distm(
+  x=cbind(dest_summarized$long,dest_summarized$lat),
+  y=cbind(rss_df$lng,rss_df$lat)) %>%
   measurements::conv_unit('m','mi') * 0.0001 #scale down for numeric performance in solver
 
 
@@ -77,10 +87,11 @@ mymodel <- ompr::MILPModel() %>%
                type='continuous',lb=0) %>%
   add_variable(zipdest_popn[dest], dest=1:destcount,type='continuous',lb=0) %>%
   add_variable(zipdest_open[dest],dest=1:destcount,type='binary') %>%
+  add_variable(dest_rss_assign[dest,rss],dest=1:destcount,rss=1:rsscount,type='binary') %>%
   
 #constraint: for each orig zip, sum of metric equals metric
   add_constraint(
-    sum_expr(zip_dest_assign[orig,dest],dest=1:destcount) == sector_zips$metric[orig],
+    sum_expr(zip_dest_assign[orig,dest],dest=1:destcount) == rs_zips$metric[orig],
     orig = 1:zipcount) %>%
 
 #constraint: for each dest zip, relate zipdest_popn to sum of assigned popn
@@ -101,13 +112,32 @@ add_constraint(zipdest_popn[dest] >= (1-avg_sector_metric_pct_tolerance) *
 add_constraint(zipdest_popn[dest] <= (1+avg_sector_metric_pct_tolerance) * 
                  avg_sector_metric * zipdest_open[dest], dest=1:destcount) %>%
   
-  
-#objective function: minimize zipzipdist * assigned volume
+  #constraint: each dest assigned to 0 RSSs if dest is not opened; 1 if opened
+  add_constraint(sum_expr(dest_rss_assign[dest,rss],rss=1:rsscount)== zipdest_open[dest], 
+                 dest=1:destcount) %>%
 
-  set_objective(sum_expr( ompr::colwise(
+  #constraint: each RSS has same sector count as recruiters assigned
+  add_constraint(sum_expr(dest_rss_assign[dest,rss],dest=1:destcount) == rss_df$sectors[rss],
+                 rss=1:rsscount) %>%
+  
+#objective function:
+
+  set_objective(
+    #minimize zip_dest_dist * assigned volume
+    sum_expr( ompr::colwise(
     array_2d_multiplication_fcn(static_array=zip_dest_dist,
 row_variable=orig,column_variable=dest)) *
-  zip_dest_assign[orig,dest], orig=1:zipcount,dest=1:destcount),sense='min')
+  zip_dest_assign[orig,dest], orig=1:zipcount,dest=1:destcount) +
+  
+  #minimize dest_rss_dist * dest to rss assignments
+  
+  sum_expr( ompr::colwise(
+    array_2d_multiplication_fcn(static_array=dest_rss_dist,
+                                row_variable=dest,column_variable=rss)) *
+      dest_rss_assign[dest,rss],dest=1:destcount,rss=1:rsscount)
+  
+  
+  ,sense='min')
 
 solve_out <-  mymodel %>% solve_model(highs_optimizer(control=highs_solver_parameters))
 
@@ -133,7 +163,7 @@ dplyr::group_by(orig) %>%
 origin_soln <- zip_dest_assign_soln %>% dplyr::group_by(orig )%>%
   dplyr::summarise(solver_metric=sum(value)) %>%
   dplyr::rowwise() %>%
-  dplyr::mutate(metric = sector_zips$metric[orig]) %>%
+  dplyr::mutate(metric = rs_zips$metric[orig]) %>%
   dplyr::mutate(absdiff = abs(solver_metric-metric))
 
 summary(origin_soln$absdiff)
@@ -151,6 +181,9 @@ summary(dest_soln$pct)
 dest_open_soln <- get_solution(solve_out, zipdest_open[dest]) %>%
   dplyr::mutate(value=round(value)) %>%  dplyr::filter(value==1)
 
+dest_rss_soln <- get_solution(solve_out, dest_rss_assign[dest,rss]) %>%
+  dplyr::mutate(value=round(value)) %>%  dplyr::filter(value==1)
+
 #mapping
 
 #colors <- RColorBrewer::brewer.pal(n=sector_count,"Set1")
@@ -165,7 +198,7 @@ zip_df <- zip_dest_assign_soln %>%
   dplyr::filter(dplyr::row_number() == 1) %>%
   dplyr::left_join(colordf) %>%
   dplyr::mutate(colors = dplyr::if_else(count_dest>1,"black",colors)) %>%
-  dplyr::left_join(sector_zips %>% 
+  dplyr::left_join(rs_zips %>% 
                      dplyr::ungroup() %>%
                      dplyr::mutate(orig = dplyr::row_number())) %>%
   dplyr::select(orig, zip,lat,long,metric,dest,count_dest,colors)
@@ -183,7 +216,7 @@ zip_df <- zip_dest_assign_soln %>%
 
 postal.codes=sf::st_read(
   "C:/Users/r_j_a/Documents/USMC/MCRC Structure/tl_2019_us_zcta510/tl_2019_us_zcta510.shp") %>%
-  dplyr::filter(ZCTA5CE10 %in% sector_zips$zip)
+  dplyr::filter(ZCTA5CE10 %in% rs_zips$zip)
 
 postal.codes2 <- postal.codes %>% 
   dplyr::left_join(zip_df %>% 
